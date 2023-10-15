d61ced03ed194bbfe59751cdcecdcf835c63b470
yaman jain <jyaman694@gmail.com>
2023-10-15 13:28:58 +0530

    fix error

--- realstate/src/App.tsx
+++ realstate/src/App.tsx
@@ -10,7 +10,6 @@ import CreateList from './pages/createList'
 import UpdateList from './pages/updateList'
 import Listing from './pages/Listing'
 import Search from './pages/Search'
-import Footer from './components/footer'
 function App() {
   return <BrowserRouter>
     <style>
